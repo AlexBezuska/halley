fd360a716cc5a41e7290b611493eaa4669b333d4
Alex Bezuska <abezuska@gmail.com>
2014-03-01 11:52:01 -0500

    clean up javascript

--- halley.js
+++ halley.js
@@ -1,28 +1,34 @@
- 
- var Blog = new Meteor.Collection("blogPosts");
+var Blog = new Meteor.Collection("blogPosts");
 
+if (Meteor.isClient) {
 
+  /* Load database content */
+   Template.posts.post = function (){
+      return Blog.find( {}, { sort :{"dateTimeStamp": -1}} );
+   }
 
+  Template.sidebar.postTitle = function (){
+      return Blog.find( {}, { sort :{"dateTimeStamp": -1}} );
+   }
 
-
- if (Meteor.isClient) {
-
-var putDate = function (){
-  var today = new Date();
-  var dd = today.getDate(); 
-  var mm = today.getMonth()+1; 
-  var yyyy = today.getFullYear();
-  return mm +"."+ dd  +"."+ yyyy; 
-};
-
+ /* tools */
+  var putDate = function (){ 
+    var today = new Date(),
+           dd = today.getDate(), 
+           mm = today.getMonth()+1,
+         yyyy = today.getFullYear();
+    return mm +"."+ dd  +"."+ yyyy; 
+  };
 
   Template.form.events({
       'click #btnSubmit' : function (){
-        var title = $('#txtTitle');
-        var body = $('#txaBody');
-        var tags = $('#txtTags');
-        var date = putDate();
+        var title = $('#txtTitle'),
+             body = $('#txaBody'),
+             tags = $('#txtTags'),
+             date = putDate();
+
         console.log(date);
+
         Blog.insert({ 
           title : title.val(),
           body  : body.val(),
@@ -37,8 +43,7 @@ var putDate = function (){
       }
   });
 
-
-Template.header.events({
+  Template.header.events({
     'click #showForm' : function () {
       $('.form').toggle();
     },
@@ -54,8 +59,7 @@ Template.header.events({
     var obj = {};
         obj[field] = target;
     Blog.update( { _id: this._id },{ $set: obj } );
-   
-  },
+  });
 
   'click .postDeleteButton' : function () {
       var removing = confirm("Are you sure you want to remove this entry? Action cannot be undone.");
@@ -66,39 +70,4 @@ Template.header.events({
         return false;
       }
     }
-  });
-
-
-   Template.posts.post = function (){
-      //return Blog.find({});
-      return Blog.find(
-        {},
-        { sort :{"dateTimeStamp": -1}} 
-      );
-   }
-
-
-
-
-
- Template.sidebar.postTitle = function (){
-      //return Blog.find({});
-      return Blog.find(
-        {},
-        { sort :{"dateTimeStamp": -1}} 
-      );
-   }
-
- Template.post.rendered = function(){
-  document.addEventListener('DOMContentLoaded', function() {
-   $('textarea').autoResize();
-});
-    
-
-
-  }
- 
-}
-
-
- 
+} // is client
